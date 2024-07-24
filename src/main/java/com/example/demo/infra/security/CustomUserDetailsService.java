318223295e305cc3d98f1300d76822d3ffbdda0c
kopz <gustavoeduardo438@gmail.com>
2024-07-24 20:52:45 -0300

    feat: security config

--- src/main/java/com/example/demo/infra/security/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/infra/security/CustomUserDetailsService.java
@@ -1,2 +1,23 @@
-package com.example.demo.infra.security;public class CustomUserDetailsService {
+package com.example.demo.infra.security;
+
+import com.example.demo.domain.user.User;
+import com.example.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+
+  @Autowired
+  private UserRepository userRepository;
+  @Override
+  public UserDetails loadUserByUsername (String username) throws UsernameNotFoundException {
+    User user = this.userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    return new org.springframework.security.core.userdetails.User(user.getEmail() , user.getPassword() , new ArrayList<>());
+  }
 }
