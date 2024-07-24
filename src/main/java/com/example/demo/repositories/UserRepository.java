e4639ab37fed2c95681a6082e39ba0462a8d1be4
kopz <gustavoeduardo438@gmail.com>
2024-07-24 18:52:56 -0300

    feat:  user class + db configuration

--- src/main/java/com/example/demo/repositories/UserRepository.java
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -1,2 +1,7 @@
-package com.example.demo.repositories;public class UserRepository {
+package com.example.demo.repositories;
+
+import com.example.demo.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
 }
