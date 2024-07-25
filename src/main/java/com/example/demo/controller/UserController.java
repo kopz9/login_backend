1752611b249b01b985e030b86a932545751ee4bc
kopz <gustavoeduardo438@gmail.com>
2024-07-24 22:05:51 -0300

    feat: user controller

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,2 +1,16 @@
-package com.example.demo.controller;public class UserController {
+package com.example.demo.controller;
+
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+  @GetMapping
+  public ResponseEntity<String> getUser(){
+    return ResponseEntity.ok("Success");
+  }
 }
