2ad38b3e1661a7b6349a2783c180b1b04500a908
kopz <gustavoeduardo438@gmail.com>
2024-07-24 22:06:04 -0300

    feat: cors config

--- src/main/java/com/example/demo/infra/cors/CorsConfig.java
+++ src/main/java/com/example/demo/infra/cors/CorsConfig.java
@@ -1,2 +1,15 @@
-package com.example.demo.infra.cors;public class CorsConfig {
+package com.example.demo.infra.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings (CorsRegistry registry) {
+    registry.addMapping("/**").allowedOrigins("https://localhost:4200").allowedMethods("GET" , "POST");
+  }
+
 }
